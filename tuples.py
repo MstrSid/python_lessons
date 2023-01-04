b3abd92452597ce0188e5f1001413d84bea54553
Eugene Kos <mstrsid@live.com>
2023-01-04 11:39:05 +0300

    tuples

---
+++ tuples.py
@@ -0,0 +1,6 @@
+# Create tuple with elements misc types
+# Create another tuple with elements misc types
+first_tuple = ('a', 1, 2.5, False)
+second_tuple = ('b', True, 2.1, 1)
+res_tuple = first_tuple + second_tuple
+print(res_tuple)
