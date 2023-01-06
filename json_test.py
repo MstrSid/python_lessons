1b6198fb0f2455f3bc8f7770642cc7571998639d
Eugene Kos <mstrsid@live.com>
2023-01-06 09:51:34 +0300

    add json task

---
+++ json_test.py
@@ -0,0 +1,18 @@
+# Create dict with values different types
+# Convert dict in json
+# Output result json in terminal
+# Output type result json in terminal
+import json
+
+some_dict = {
+    'id': 15,
+    'number': 2.5,
+    'some_data': {
+        's1': 'String',
+        'a2': [1, 2, 3]
+    }
+}
+
+json_from_dict = json.dumps(some_dict, indent=2)
+print(json_from_dict)
+print(type(json_from_dict))
